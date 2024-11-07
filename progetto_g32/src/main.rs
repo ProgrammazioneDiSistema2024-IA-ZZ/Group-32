8dd5b3c4adfc7241a5e99500afbf743a68fc8326
Matteo Petrera s331356 <s331356@studenti.polito.it>
2024-11-07 15:05:19 +0100

    implemented partially service-manager

--- progetto_g32/src/main.rs
+++ progetto_g32/src/main.rs
@@ -1,9 +1,13 @@
+use service_manager::native_service_manager;
+
 mod mouse_input;
 mod audio;
+mod setup;
+mod uninstall;
 
 fn main() {
     println!("avvio programma");
     mouse_input::main();
-    //confirmation_main();
+    //confirmation_main()
 }
 
