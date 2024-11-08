dd2c36c79cdbbb885a46ea4dd7d9eff9e2e14922
Matteo Petrera s331356 <s331356@studenti.polito.it>
2024-11-08 17:38:17 +0100

    audio path (absolute)

--- progetto_g32/src/audio.rs
+++ progetto_g32/src/audio.rs
@@ -6,7 +6,7 @@ pub fn play_sound() {
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
     let sink = Sink::try_new(&stream_handle).unwrap();
 
-    let file = BufReader::new(File::open("audio_backup/audio.wav").unwrap());
+    let file = BufReader::new(File::open("/Users/matteopetrera/Desktop/POLITO/MAGISTRALE/23-24-2semestre/PDS/RUST/Group-32/progetto_g32/audio_backup/audio.wav").unwrap());
     let source = rodio::Decoder::new(file).unwrap();
     sink.append(source);
 
